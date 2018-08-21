4571e90f715de663013e883d4259d4818ce224c2
ihsan97 <40269169+ihsan97@users.noreply.github.com>
2018-08-21 16:05:08 +0530

    Update xxd.py

--- xxd.py
+++ xxd.py
@@ -1,32 +1,56 @@
-import sys
-import os.path
+from sys import argv
 
-def xxd(file_path):
-    f=open(file_path,'r')
-    t=0
-    while True:
-        r=f.read(16)
-        d=r
-        if not r:
-            break
-        a=[]
-        for c in r:
-            a.append('%02x'%ord(c))
-        b=[]
-        for z in range(0,len(a),2):
-            b.append(''.join((a[z:z+2])))
-        kk=[]
-        for i in d:
-            ct=ord(i)
-            if ct<32 or ct>127:
-                kk.append('.')
-            else:
-                kk.append(i)
-        step=('%08x'%(t*16))
-        print('{0}: {1:<39} {2}'.format(step,' '.join(b),''.join(kk)))
-        t=t+1
+text_list=[]
+
+script, file=argv
+fo=open(file,"r")                               #Open the given file in command line
+textin=fo.read()
+fo.close()
 
 
-   
-    sys.exit(1)
-xxd(sys.argv[1])
+def func(text):                                 #Convert ASCII strings to hex values
+        string=""
+        for i in str(text):
+                h=str(hex(ord(i)))[2:]
+                string+=h
+                
+        count=0
+        string2=""
+        for j in str(string):   
+                count+=1
+                string2+=j
+                if count%4==0:
+                        string2+=" "
+        return string2
+
+def replace_line(word):
+        new_word=""
+        for i in str(word):
+                if i=="\n" or i==" ":           #Replace Enter and Space with dot
+                        new_word+="."
+                else:
+                        new_word+=i
+        
+        return new_word
+
+def line(val):                                  #Setting Couter values
+        return "0"*(7-len(str(hex(val))[2:]))+str(hex(val))[2:]+"0"
+
+        
+text=""
+count=0
+for i in str(textin):                           #Break lines after 16 characters
+        text+=i
+        count+=1
+        if count == 16:       
+                text_list.append(text)
+                count=0
+                text=""
+                
+else:
+        text_list.append(text)
+        text=""
+
+
+for i in range(len(text_list)):                 #print those defined functions to get the output
+        print(line(i),func(text_list[i]),str(replace_line(text_list[i])))
